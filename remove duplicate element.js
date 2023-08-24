28f6c42ab47acede6cc8a9b67f35b7a71fb4ca8f
assim fayas <asim1fayas@gmail.com>
2023-08-24 10:42:40 +0530

    leet code 26 ,remove duplicate from sorted array

---
+++ remove duplicate element.js
@@ -0,0 +1,17 @@
+// 26. Remove Duplicates from Sorted Array
+
+
+var removeDuplicates = function(nums) {
+    let len = nums.length
+    let digits = 1;
+
+    for (let i = 1; i < len; i++)
+    {
+        if (nums[i] != nums[i - 1])
+        {
+            nums[digits] = nums[i];
+            digits++;
+        }
+    }
+    return digits;
+};
